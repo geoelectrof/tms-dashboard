1cce7c8dc6b7e97743d714f882287efc717dd4fd
Dimitris Georgakopoulos <dimitris.geo@gmail.com>
2023-05-29 11:05:36 +0300

    Setup of sass color variables and basic shipments page styling

--- src/components/Shipments.jsx
+++ src/components/Shipments.jsx
@@ -1,29 +1,46 @@
 import { useSelector } from "react-redux"
 import { LinkContainer } from "react-router-bootstrap"
+import { Container, Row, Col, Button } from "react-bootstrap"
 
 const Shipments = () => {
     const { shipments } = useSelector(state => state.shipments)
     console.log('shipments', shipments)
 
     return (
-        <>
-            <div>Shipments</div>
-            {shipments.map(shipment => {
-                // console.log('shipment', shipment)
-                return (
-                    <div key={shipment.id}>
-                        <h1>{shipment.id}</h1>
-                        <h2>{shipment.customerName}</h2>
-                        <LinkContainer to={`/shipments/${shipment.id}`}>
-                            <button>
-
-                            More info
-                            </button>
-                        </LinkContainer>
-                    </div>
-                )
-            })}
-        </>
-    )
+      <Container>
+        <Row>
+          <Col>
+            <h1 className="fw-bold">Shipments</h1>
+            <p className="text-secondary">7 total shipments</p>
+          </Col>
+          <Col className=" d-flex align-items-center justify-content-end">
+            <select name="status" id="status">
+              <option value="Filter by">Filter by</option>
+              <option value="In transit">In transit</option>
+              <option value="Delivered">Delivered</option>
+              <option value="Delayed">Delayed</option>
+            </select>
+            <Button
+              variant="primary text-white fw-bold w-5 rounded-pill"
+              className="new-invoice-btn"
+            >
+              + New Invoice
+            </Button>
+          </Col>
+        </Row>
+        {shipments.map((shipment) => {
+          // console.log('shipment', shipment)
+          return (
+            <div key={shipment.id}>
+              <h1>{shipment.id}</h1>
+              <h2>{shipment.customerName}</h2>
+              <LinkContainer to={`/shipments/${shipment.id}`}>
+                <button>More info</button>
+              </LinkContainer>
+            </div>
+          );
+        })}
+      </Container>
+    );
 }
 export default Shipments
--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -4,7 +4,14 @@ const Summary = () => {
   return (
     <div>
         Summary
-        <Button variant="primary">Hello world</Button>
+        <Button variant="primary text-white fw-light">Hello world</Button>
+        <Button variant="secondary text-white fw-bold">Hello world</Button>
+        <Button variant="white text-secondary">Hello world</Button>
+        <Button variant="danger text-secondary">Hello world</Button>
+        <Button variant="warning text-secondary">Hello world</Button>
+        <Button variant="info text-white">Hello world</Button>
+        <Button variant="dark text-secondary">Hello world</Button>
+        <Button variant="light text-secondary">Hello world</Button>
     </div>
   )
 }
